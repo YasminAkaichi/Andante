87996bb1e8a6bad7e3bddd852ab17224c02fee80
Simon Jacquet <simon.jacquet@unamur.be>
2022-08-30 23:18:06 +0200

    Updated setup.py
    - Packages are now automatically installed when installing the andante
      package
    - requirements.txt is now deprecated and removed

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='andante', version='1.0', packages=find_packages())
+setup(
+    name='andante', 
+    version='1.0', 
+    packages=find_packages(),
+    install_requires=[
+        "parsimonious>=0.8.1",
+        "dataclasses>=0.6",
+        "ipywidgets>=7.6.5",
+    ],
+)
